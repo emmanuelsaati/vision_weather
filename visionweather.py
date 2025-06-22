b4ba135d4d9182d31ca0b04a8244d9635caee66d
emmanuelsaati <111987325+emmanuelsaati@users.noreply.github.com>
2025-06-22 07:23:37 +0000

    Update visionweather.py

--- visionweather.py
+++ visionweather.py
@@ -1,3 +1,8 @@
+
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 from src.predict import predict_weather
 
 print(predict_weather('test.jpg'))
